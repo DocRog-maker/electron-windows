7a7969234705c904339c18509d2316d261b864e3
Roger Dunham <roger.dunham@apryse.com>
2025-12-01 13:09:03 +1300

    RC simple app

--- main.js
+++ main.js
@@ -1,33 +1,18 @@
 const { app, BrowserWindow } = require('electron');
 const log = require('electron-log');
 
-let mainWindow;
-const gotLock = app.requestSingleInstanceLock();
-
-if (!gotLock) {
-  app.quit();
-} else {
-  app.on('second-instance', (event, argv) => {
-    log.info('Second instance launched with args:', argv);
-    // argv contains the file path if a PDF was double-clicked
-    const filePath = argv.find(arg => arg.endsWith('.pdf'));
-    if (filePath && mainWindow) {
-      mainWindow.webContents.send('open-pdf', filePath);
+app.on('ready', () => {
+  log.info('App is ready');
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: __dirname + '/preload.js',
+      contextIsolation: true
     }
   });
 
-  app.on('ready', () => {
-    log.info('App is ready');
-    mainWindow = new BrowserWindow({
-      webPreferences: {
-        preload: __dirname + '/preload.js',
-        contextIsolation: true
-      }
-    });
+  //Hide the default title bar
+  mainWindow.setMenuBarVisibility(false);
+  mainWindow.loadFile('index.html')
 
-    //Hide the default title bar
-    mainWindow.setMenuBarVisibility(false);
-    mainWindow.loadFile('index.html')
+});
 
-  });
-}
--- preload.js
+++ preload.js
@@ -1,6 +1,4 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  // Listen for PDFs opened via OS
-  onOpenPdf: (callback) => ipcRenderer.on('open-pdf', (event, path) => { console.log(`path ${path}`); callback(path) }),
 });
